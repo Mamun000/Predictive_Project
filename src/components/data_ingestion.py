c6e1737b7a182806d43d0e5323a9e70038f1e301
Mamun000 <skmamunislam786@gmail.com>
2025-10-31 10:16:28 +0530

    Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,11 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass        #this is a decorator
 class DataIngestionConfig:
@@ -19,7 +24,7 @@ class DataIngestion:
         self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info('Entered the data ingestion method or component')
+        logging.info("Entered the data ingestion method or component")
         try:
             df=pd.read_csv("notebook\data\stud.csv")       #here we can read from anywhere mongodb or any other data source
             logging.info("Read the dataset as dataframe")
@@ -47,7 +52,13 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
     
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
     
+
 
 
         
